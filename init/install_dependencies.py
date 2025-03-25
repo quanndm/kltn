2d48e0af101ae72e8bef12359258ef7cb1103d3d
quanndm <minhquan1852@gmail.com>
2025-03-25 13:33:12 +0700

    update install_dependencies file

--- init/install_dependencies.py
+++ init/install_dependencies.py
@@ -2,5 +2,6 @@ import os
 
 def install_packages(path_requires):
     path_req = os.path.join(path_requires, 'requirements.txt')
+    os.system('pip uninstall -y numpy')
     os.system(f'pip install -r {path_req}')
     
