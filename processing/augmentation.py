92acc90a7ab7ba82576446d1acb8ab74ad3294e2
quanndm <minhquan1852@gmail.com>
2025-04-01 11:30:43 +0700

    update augmentation file

--- processing/augmentation.py
+++ processing/augmentation.py
@@ -5,10 +5,8 @@ def train_augmentations():
     Returns a list of augmentations to apply to the training data
     Augmentations:
         RandRotate90d: Randomly rotate the image by 90 degrees
-        ScaleIntensityRanged: Scale the intensity of the image to a given range, in this case from -200 to 200 - like truncate HU from -200 to 200
     """
     return Compose([
         RandRotate90d(keys=["image", "label"], prob=0.5, max_k=1),
-        ScaleIntensityRanged(keys=["image"], a_min=-200, a_max=200, b_min=0.0, b_max=1.0, clip=True),
     ])
     
