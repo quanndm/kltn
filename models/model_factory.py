9fdaf08d354c904375238e39c4c7fdc86a356ba0
quanndm <minhquan1852@gmail.com>
2025-04-04 09:20:13 +0700

    add model factory

---
+++ models/model_factory.py
@@ -0,0 +1,28 @@
+from unet3d  import UNet3D
+from unet3d_cot import UNet3DWCoT
+from unet3d_cot_da import UNet3DWCoTDA
+from unet3d_rescot_da import UNet3DWResCoTDA
+from unet3d_resnextcot_da import UNet3DWResNeXtCoTDA
+
+class ModelFactory:
+    _model = {
+        "unet3d": UNet3D,
+        "unet3d_cot": UNet3DWCoT,
+        "unet3d_cot_da": UNet3DWCoTDA,
+        "unet3d_rescot_da": UNet3DWResCoTDA,
+        "unet3d_resnextcot_da": UNet3DWResNeXtCoTDA,
+    }
+
+    @staticmethod
+    def get_model(model_name, in_channels, n_classes, n_channels):
+        """
+        Get the model class based on the model name.
+        :param model_name: Name of the model to retrieve.
+        :param in_channels: Number of input channels.
+        :param n_classes: Number of output classes.
+        :param n_channels: Number of channels in the model.
+        :return: An instance of the specified model.
+        """
+        if model_name not in ModelFactory._model:
+            raise ValueError(f"Model {model_name} not found!")
+        return ModelFactory._model[model_name](in_channels, n_classes, n_channels)
