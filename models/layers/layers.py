0518da17b39a1d508779c4aefee3a0f307182ef9
quanndm <minhquan1852@gmail.com>
2025-04-04 09:53:42 +0700

    update layers

--- models/layers/layers.py
+++ models/layers/layers.py
@@ -185,11 +185,12 @@ class ResCoTAttention(nn.Module):
         return out
 
 class ResNeXtCoTBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, ):
+    def __init__(self, in_channels, out_channels, cardinality = 32, bottleneck_width = 4):
         super(ResNeXtCoTBlock, self).__init__()
-        mid_channels = out_channels // 2
+        # mid_channels = out_channels // 2
         self.relu = nn.ReLU(inplace=True)
-        cardinality = self.get_suitable_cardinality(out_channels)
+        inner_channels = bottleneck_width * cardinality
+
         self.conv1 = nn.Sequential(
 
             nn.Conv3d(in_channels, mid_channels, kernel_size=1, bias=False),
@@ -198,19 +199,19 @@ class ResNeXtCoTBlock(nn.Module):
         )
 
         self.conv2 = nn.Sequential(
-            nn.Conv3d(mid_channels, mid_channels, kernel_size=3, stride=1, padding=1, groups=cardinality, bias=False),
+            nn.Conv3d(inner_channels, inner_channels, kernel_size=3, stride=1, padding=1, groups=cardinality, bias=False),
             nn.BatchNorm3d(mid_channels),
             self.relu
         )
 
         self.conv3 = nn.Sequential(
-            CoTAttention(mid_channels, 3),
-            nn.BatchNorm3d(mid_channels),
+            CoTAttention(inner_channels, 3),
+            nn.BatchNorm3d(inner_channels),
             self.relu
         )
 
         self.conv4 = nn.Sequential(
-            nn.Conv3d(mid_channels, out_channels, kernel_size=1, bias=False),
+            nn.Conv3d(inner_channels, out_channels, kernel_size=1, bias=False),
             nn.BatchNorm3d(out_channels)
         )
 
@@ -235,12 +236,3 @@ class ResNeXtCoTBlock(nn.Module):
         out = self.relu(out)
 
         return out
-
-    @staticmethod
-    def get_suitable_cardinality(out_channels):
-        min_channels_per_group = 4
-        max_card = out_channels // min_channels_per_group
-        for c in reversed(range(1, max_card + 1)):
-            if out_channels % c == 0:
-                return c
-        return 1
