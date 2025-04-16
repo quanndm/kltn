3d469d5975849fa81b1e1213376b2b555d883552
quanndm <minhquan1852@gmail.com>
2025-04-16 08:39:58 +0700

    update model, notebooks

--- models/model_factory.py
+++ models/model_factory.py
@@ -2,6 +2,7 @@ from .unet3d  import UNet3D, UNet3DPretrained
 from .unet3d_cot import UNet3DWCoT, UNet3DWCoTPretrained
 from .unet3d_cot_da import UNet3DWCoTDA, UNet3DWCoTDAPretrained
 from .unet3d_resnextcot import UNet3DWResNeXtCoT, UNet3DWResNeXtCoTPretrained
+from .unet3d_convnextv2cot import UNet3DWConvNeXtV2CoT, UNet3DWConvNeXtV2CoTPretrained
 from .unet3d_resnextcot_da import UNet3DWResNeXtCoTDA, UNet3DWResNeXtCoTDAPretrained
 from .unet3d_convnextv2cot_da import UNet3DWConvNeXtV2CoTDA, UNet3DWConvNeXtV2CoTDAPretrained
 
@@ -11,6 +12,7 @@ class ModelFactory:
         "unet3d_cot": UNet3DWCoT,
         "unet3d_resnextcot": UNet3DWResNeXtCoT,
         "unet3d_cot_da": UNet3DWCoTDA,
+        "unet3d_convnextv2_cot": UNet3DWConvNeXtV2CoT,
         "unet3d_resnextcot_da": UNet3DWResNeXtCoTDA,
         "unet3d_convnextv2cot_da": UNet3DWConvNeXtV2CoTDA,
     }
@@ -19,6 +21,7 @@ class ModelFactory:
         "unet3d": UNet3DPretrained,
         "unet3d_cot": UNet3DWCoTPretrained,
         "unet3d_resnextcot": UNet3DWResNeXtCoTPretrained,
+        "unet3d_convnextv2_cot": UNet3DWConvNeXtV2CoTPretrained,
         "unet3d_cot_da": UNet3DWCoTDAPretrained,
         "unet3d_resnextcot_da": UNet3DWResNeXtCoTDAPretrained,
         "unet3d_convnextv2cot_da": UNet3DWConvNeXtV2CoTDAPretrained,
