5c27e2e4324cc4c9e8f23f76426b39bee55d264e
quanndm <minhquan1852@gmail.com>
2025-04-19 10:10:09 +0700

    update train_val_epochs

--- utils/train_val_epochs.py
+++ utils/train_val_epochs.py
@@ -265,11 +265,11 @@ def trainer_stage2(model, train_loader, val_loader, optimizer, loss_func, acc_fu
             logger.info(f"\n{'*' * 20}Epoch {epoch} Validation{'*' * 20}")
             val_acc , val_ious, val_precisions, val_recalls= val_epoch_stage2(model, val_loader, epoch, acc_func, max_epochs, logger)
 
-            val_dice_tumor = val_acc[0]
+            val_dice_tumor = val_acc
 
-            val_iou_tumor = val_ious[0]
+            val_iou_tumor = val_ious
 
-            val_precision_tumor = val_precisions[0]
+            val_precision_tumor = val_precisions
 
             val_recall_tumor = val_recalls[0]
             logger.info(f"\n{'*' * 20}Epoch Summary{'*' * 20}")
