3dc92af24e84c5260359469389be5c66006d6db2
quanndm <minhquan1852@gmail.com>
2025-04-21 14:58:13 +0700

    update package

--- init/install_dependencies.py
+++ init/install_dependencies.py
@@ -4,6 +4,7 @@ import yaml
 def install_packages(path_requires):
     path_req = os.path.join(path_requires, 'requirements.txt')
     os.system('pip uninstall -y numpy')
+    os.system('pip install numpy==1.26.4')
     os.system(f'pip install -r {path_req}')
 
 def load_config(config_file):
