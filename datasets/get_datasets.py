2f3ea3cadd8955d5b4d8b947c8955d5b6efc25e0
quanndm <minhquan1852@gmail.com>
2025-04-28 09:49:14 +0700

    update get datasets

--- datasets/get_datasets.py
+++ datasets/get_datasets.py
@@ -100,7 +100,7 @@ def get_liver_mask(source_folder, model_stage_1=None, device=None):
             liver_mask = extract_liver_mask_binary(logits, threshold=0.5)[0]
             liver_mask = keep_largest_connected_component(liver_mask)
             liver_mask = smooth_mask(liver_mask, kernel_size=3)
-        liver_masks.append(liver_mask)
+        liver_masks.append(liver_mask.cpu().numpy())
 
         torch.cuda.empty_cache()
         del image
