6fd07fb0345de61d417f48298f9b807358c7fd62
quanndm <minhquan1852@gmail.com>
2025-04-29 23:44:52 +0700

    update lits

--- datasets/lits.py
+++ datasets/lits.py
@@ -207,16 +207,16 @@ class Stage2Dataset(Dataset):
         image = truncate_HU(image)
 
         # normalizations
-        # if normalizations == "zscores":
-        #     image = zscore_normalise(image)
-        # else:
-        #     image = normalize(image)        
+        if normalizations == "zscores":
+            image = zscore_normalise(image)
+        else:
+            image = normalize(image)        
 
         # expand dims of image and segmentation and resize image
         image, seg= np.expand_dims(image, axis=0), np.expand_dims(seg, axis=0)
 
         # expand dims of image and segmentation and resize image
-        image, seg = resize_image(image, seg, target_size=(128, 128, 128))  
+        # image, seg = resize_image(image, seg, target_size=(128, 128, 128))  
         return image, seg
     @staticmethod
     def augmentation(image, seg):
