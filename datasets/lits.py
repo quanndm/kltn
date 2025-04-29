26c3c6547c0834290b8a6c48c31705bad98af2ac
quanndm <minhquan1852@gmail.com>
2025-04-29 14:21:13 +0700

    update lits

--- datasets/lits.py
+++ datasets/lits.py
@@ -161,11 +161,7 @@ class Stage2Dataset(Dataset):
         liver_mask = (seg == 1).astype(np.uint8)
         seg = (seg == 2).astype(np.uint8)
         # convert to torch tensors
-        if self.training:
-            image, seg = torch.from_numpy(image.detach().cpu().numpy()), torch.from_numpy(seg.detach().cpu().numpy())
-        else:
-            image, seg = torch.from_numpy(image), torch.from_numpy(seg)
-
+        image, seg = torch.from_numpy(image), torch.from_numpy(seg)
         liver_mask  = torch.from_numpy(liver_mask)
         return dict(
             idx=idx,
