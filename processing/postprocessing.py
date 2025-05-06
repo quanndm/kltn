018be7d71d2598d76e10eb06d5c7a407917b6e15
quanndm <minhquan1852@gmail.com>
2025-05-06 16:14:47 +0700

    update post process

--- processing/postprocessing.py
+++ processing/postprocessing.py
@@ -32,7 +32,7 @@ def post_processing_stage2(logits, threshold=0.5, device=None):
     """
     Post-processing for stage 2 of the model.
     Args:
-        logits (torch.Tensor): The logits output from the model, shape (1, 1, D, H, W).
+        logits (torch.Tensor): The logits output from the model, shape  1, H, W).
         threshold (float): The threshold for binarizing the logits.
     Returns:
         torch.Tensor: The post-processed logits.
@@ -43,11 +43,11 @@ def post_processing_stage2(logits, threshold=0.5, device=None):
     # Convert to numpy array and apply threshold
     tumor_mask = probs.cpu().numpy()
     tumor_mask =(tumor_mask > threshold).astype(np.uint8)
-    tumor_mask = np.squeeze(tumor_mask) # shape(1, 1, D, H, W) -> (D, H, W)
+    tumor_mask = np.squeeze(tumor_mask) # shape(1, H, W) -> (H, W)
 
     # Morphological closing and opening
-    tumor_mask = morphology.binary_closing(tumor_mask, morphology.ball(1))
-    tumor_mask = morphology.binary_opening(tumor_mask, morphology.ball(1))
+    tumor_mask = morphology.binary_closing(tumor_mask, morphology.disk(3))
+    tumor_mask = morphology.binary_opening(tumor_mask, morphology.disk(3))
 
     # fill holes
     tumor_mask = ndi.binary_fill_holes(tumor_mask)
