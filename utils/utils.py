d49b9e3f57cb43415516b38f88e937eb348c431a
quanndm <minhquan1852@gmail.com>
2025-05-07 10:45:27 +0700

    update utils

--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,7 @@ from monai.inferers import sliding_window_inference
 
 # Assuming the variables roi_size, sw_batch_size, and overlap are lists
 roi = (128, 128, 128)
+roi_2d = (3, 256, 256) 
 sw_batch_size = 1
 overlap = 0.5
 
@@ -30,10 +31,43 @@ def inference(input, model):
     def _compute(input):
         return sliding_window_inference(
             inputs=input,
-            roi_size=(128, 128, 128),
-            sw_batch_size=1,
+            roi_size=roi
+            sw_batch_size=sw_batch_size,
             predictor=model,
-            overlap=0.5,
+            overlap=overlap,
+        )
+    if VAL_AMP:
+        with torch.autocast(device):
+            return _compute(input)
+    else:
+        return _compute(input)
+
+        
+def model_inferer_2d(input, model):
+    def _compute(input):
+        return sliding_window_inference(
+            inputs=input,
+            roi_size= roi_2d,
+            sw_batch_size=sw_batch_size,
+            predictor=model,
+            overlap=overlap,
+        )
+
+    if VAL_AMP:
+        with torch.autocast(device):
+            return _compute(input)
+    else:
+        return _compute(input)
+
+
+def inference_2d(input, model):
+    def _compute(input):
+        return sliding_window_inference(
+            inputs=input,
+            roi_size=roi_2d,
+            sw_batch_size=sw_batch_size,
+            predictor=model,
+            overlap=overlap,
         )
     if VAL_AMP:
         with torch.autocast(device):
