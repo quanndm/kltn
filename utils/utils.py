6c484261f1286f7da1c5baf1fde0ae2337f1481f
quanndm <minhquan1852@gmail.com>
2025-06-28 21:38:35 +0700

    update utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 import torch
 from monai.inferers import sliding_window_inference
 import numpy as np
-from ..processing.postprocessing import post_trans_stage1
+from ..processing.postprocessing import post_trans_stage1, post_processing_stage2
 from ..processing.preprocessing import resize_image
 
 # Assuming the variables roi_size, sw_batch_size, and overlap are lists
@@ -114,6 +114,37 @@ def find_best_slice(ct_array, mask_array=None, axis=0, threshold=0.01):
     best_index = int(np.argmax(scores))
     return best_index
 
+def paste_mask_to_full(mask_pred, bbox, full_shape):
+    """
+    Đưa mask nhỏ vào lại vị trí cũ trong CT slice full size.
+
+    Parameters:
+    - mask_pred: mask 2D đã predict, shape (h, w)
+    - bbox: [z, y1, x1, y2, x2] — vị trí crop ban đầu
+    - full_shape: (H, W) — kích thước lát CT gốc
+
+    Returns:
+    - full_mask: mask full size với vùng predict được dán vào đúng vị trí
+    """
+    y_min, y_max, x_min, x_max = bbox
+    full_mask = np.zeros(full_shape, dtype=mask_pred.dtype)
+
+    h, w = y_max - y_min, x_max - x_min
+
+    # Resize mask nếu cần thiết
+    if mask_pred.shape != (h, w):
+        mask_pred = resize(
+            mask_pred,
+            (h, w),
+            order=0,  # nearest-neighbor để giữ nhãn
+            preserve_range=True,
+            anti_aliasing=False
+        ).astype(mask_pred.dtype)
+
+    full_mask[y_min:y_max, x_min:x_max] = mask_pred
+
+    return np.expand_dims(full_mask, axis=0)
+
 def predict_and_resize_mask_stage_1(model, image, mask):
     model.eval()
     ct_prep, mask_prep = preprocessing_liver(image, mask)
@@ -129,3 +160,18 @@ def predict_and_resize_mask_stage_1(model, image, mask):
     image_liver, seg_liver  = image_liver.squeeze(), seg_liver.squeeze()
 
     return image_liver, seg_liver
+
+def predict_and_resize_mask_stage_2(model, compressed_array_path, full_shape):
+    model.eval()
+
+    image, mask_tumor, liver_mask, bbox = preprocessing_tumor(compressed_array_path)
+    ct_prep, mask_prep = image.unsqueeze(0).to(device), mask_tumor.unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        logits = model(ct_prep)
+
+    mask_pred = post_processing_stage2(logits.squeeze()).cpu().numpy()
+    ct_prep = ct_prep.squeeze().cpu().numpy()
+    
+    mask_full = paste_mask_to_full(mask_pred.squeeze(), bbox[2:6], full_shape=full_shape)
+    return mask_full
