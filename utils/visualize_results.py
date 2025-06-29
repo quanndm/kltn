f63b35b178fdaedb98cf739d205428b4fb0a2a5e
quanndm <minhquan1852@gmail.com>
2025-06-29 21:36:14 +0700

    update visualize

--- utils/visualize_results.py
+++ utils/visualize_results.py
@@ -51,51 +51,6 @@ def visualize_results(model, val_loader, weight_path, num_images, device):
         if stop == num_images:
           break
 
-# def visualize_results_stage_1(model, val_loader, weight_path, num_images, device):
-#     model.load_state_dict(torch.load(weight_path, map_location=device))
-#     model.eval()
-#     stop = 0
-
-#     cmap = mcolors.ListedColormap(["black", "yellow"])  # 0: background, 1: tumor
-
-#     for val_data in val_loader:
-#         stop += 1
-#         with torch.no_grad():
-#             val_input = val_data["image"].to(device)
-#             val_output = inference(val_input, model)  # [B, C, D, H, W]
-#             val_output = post_trans_stage1(val_output[0])    # keep [C, D, H, W] format
-
-#             image_np = val_data["image"].detach().cpu().numpy()[0, 0]  # [D, H, W]
-#             label_np = val_data["label"].detach().cpu().numpy()[0, 0]  # [D, H, W]
-#             pred_np = (val_output > 0.5).int().detach().cpu().numpy()[0]  # [D, H, W]
-
-#             # Combine all slices into one 2D image using max projection (any voxel > 0 will appear)
-#             image_2d = image_np[image_np.shape[0] // 2]  # middle slice
-#             label_2d = np.max(label_np, axis=0)
-#             pred_2d = np.max(pred_np, axis=0)
-
-#             # Show the image
-#             plt.figure(figsize=(6, 6))
-#             plt.title("Input Image")
-#             plt.imshow(image_2d, cmap="gray")
-#             plt.axis("off")
-#             plt.show()
-
-#             # Show label and prediction
-#             fig, ax = plt.subplots(1, 2, figsize=(12, 6))
-#             ax[0].imshow(label_2d, cmap=cmap, vmin=0, vmax=1)
-#             ax[0].set_title("Ground Truth (liver)")
-#             ax[0].axis("off")
-
-#             ax[1].imshow(pred_2d, cmap=cmap, vmin=0, vmax=1)
-#             ax[1].set_title("Prediction (liver)")
-#             ax[1].axis("off")
-
-#             plt.tight_layout()
-#             plt.show()
-
-#         if stop == num_images:
-#             break
 
 def visualize_results_stage_2(model, val_loader, weight_path, num_images, device, threshold=0.5):
     model.load_state_dict(torch.load(weight_path, map_location=device))
